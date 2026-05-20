6983199eb465d125c07ed67c7cf7d93623acefad
Ankit Samal <ankitsamal966@gmail.com>
2026-05-20 19:47:25 +0530

    connected custom domain

--- config/settings.py
+++ config/settings.py
@@ -128,6 +128,10 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://pytoolshub.in',
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
