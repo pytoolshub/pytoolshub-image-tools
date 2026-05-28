63e835798f3fd1974836829effdc5722846a15a1
Ankit Samal <ankitsamal966@gmail.com>
2026-05-28 22:16:07 +0530

    Added Adsense ads.txt redirect

--- config/urls.py
+++ config/urls.py
@@ -40,12 +40,9 @@ urlpatterns = [
 
     path('', include('tools.urls')),
 
-    path(
-        'sitemap.xml',
-        sitemap,
-        {'sitemaps': sitemaps},
-        name='django.contrib.sitemaps.views.sitemap'
-    ),
+    path('sitemap.xml',sitemap,{'sitemaps': sitemaps},name='django.contrib.sitemaps.views.sitemap'),
+
+    path("ads.txt",RedirectView.as_view(url=static("ads.txt"),permanent=True),),
 ]
 
 
